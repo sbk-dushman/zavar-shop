4ec02e7e2826d50a5e4816aa1f5268b291eb5414
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2023-04-30 23:16:25 +0500

    3.6 assets + task 3

---
+++ src/data/products.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    title: 'Телевизор Toshiba 50C350KE',
+    price: 29999,
+    img: 'img/tvtosh.jpg',
+  },
+  {
+    title: 'Ноутбук Lenovo IdeaPad 3 15ITL05',
+    price: 35999,
+    img: 'img/lena.jpg',
+  },
+  {
+    title: 'Робот-пылесос Redmond RV-R560',
+    price: 6999,
+    img: 'img/redmond-robot.jpg',
+  },
+  {
+    title: 'Смарт-часы Apple SE 44mm Midnight Alum / Sport M/L',
+    price: 29999,
+    img: 'img/aw.jpg',
+  },
+
+  {
+    title: 'Умная колонка Яндекс Станция Лайт с Алисой, бирюзовый мята',
+    price: 3999,
+    img: 'img/alice.jpg',
+  },
+  {
+    title: 'Ноутбук Apple MacBook Pro 16 M2',
+    price: 259999,
+    img: 'img/macbook.jpg',
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    img: 'img/toothbrush.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    img: 'img/phone.jpg',
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    img: 'img/board.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    price: 14960,
+    img: 'img/phone-2.jpg',
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 8960,
+    img: 'img/phone-3.jpg',
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39900,
+    img: 'img/bicycle.jpg',
+  },
+  {
+    title: 'Гироскутер Razor Hovertrax 2.0',
+    price: 34900,
+    img: 'img/wheels.jpg',
+  },
+  {
+    title: 'Детский трюковой самокат Razor Grom',
+    price: 4990,
+    img: 'img/scooter.jpg',
+  },
+  {
+    title: 'Роллерсёрф Razor RipStik Air Pro',
+    price: 6690,
+    img: 'img/ripstik.jpg',
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16560,
+    img: 'img/airpods.jpg',
+  },
+  {
+    title: 'Наушники Sony',
+    price: 30690,
+    img: 'img/radio.jpg',
+  },
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    img: 'img/headphones.jpg',
+  },
+];
