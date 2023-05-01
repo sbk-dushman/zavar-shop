df8ede441c609112fd81d886110034021da14267
sbk-dushman <43039866+sbk-dushman@users.noreply.github.com>
2023-05-01 23:17:21 +0500

    task 5

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,6 @@
 <template>
-      <section class="catalog">
-        <ul class="catalog__list">
-          <li class="catalog__item" v-for="(product,index) in products" :key="index">
-            <a class="catalog__pic" href="#">
-              <img :src="product.img" :alt="product.title">
-            </a>
-
-            <h3 class="catalog__title">
-              <a href="#">
-               {{ product.title}}
-              </a>
-            </h3>
-
-            <span class="catalog__price">
-              {{product.price}} ₽
-            </span>
-
-            <ul class="colors colors--black">
-              <li class="colors__item">
-                <label for="color-1" class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="">
-                  <span class="colors__value" style="background-color: #73B6EA;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000">
-                  <span class="colors__value" style="background-color: #8BE000;">
-                  </span>
-                </label>
-              </li>
-              <li class="colors__item">
-                <label class="colors__label">
-                  <input class="colors__radio sr-only" type="radio" name="color-1" value="#222">
-                  <span class="colors__value" style="background-color: #222;">
-                  </span>
-                </label>
-              </li>
-            </ul>
-          </li>
-
-        </ul>
-
+ <section class="catalog">
+        <ProductList :products="products" />
         <ul class="catalog__pagination pagination">
           <li class="pagination__item">
             <a class="pagination__link pagination__link--arrow pagination__link--disabled" aria-label="Предыдущая страница">
@@ -93,7 +51,9 @@
 </template>
 
 <script>
-import products from "./data/products";
+import products from './data/products';
+import ProductList from './components/ProductList.vue';
+
 export default {
   name: 'App',
   data() {
@@ -101,8 +61,7 @@ export default {
       products,
     };
   },
-  // components: {
-  // },
+  components: { ProductList },
 };
 </script>
 
---
+++ src/components/ProductList.vue
@@ -0,0 +1,19 @@
+<template>
+             <ul class="catalog__list">
+              <ProductItem v-for="(product,index) in products" :key="index" :product="product"/>
+
+        </ul>
+
+</template>
+
+<script>
+import ProductItem from './ProductItem.vue';
+
+export default {
+  props: ['products'],
+  components: { ProductItem },
+};
+</script>
+<style>
+
+</style>
